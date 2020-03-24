e1f649dceeb447c81b4cbda5ff9b0732ffe08f79
SmallHong <h_q_liu@163.com>
2020-03-24 15:59:09 +0800

    修改发布接龙为多步

--- resources/scripts/axure/globals.js
+++ resources/scripts/axure/globals.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 ﻿$axure.internal(function($ax) {
     var _globals = $ax.globals = {};
 
     $ax.globals.MaxZIndex = 1000;
     $ax.globals.MinZIndex = -1000;
     
-=======
-﻿$axure.internal(function($ax) {
-    var _globals = $ax.globals = {};
-
-    $ax.globals.MaxZIndex = 1000;
-    $ax.globals.MinZIndex = -1000;
-    
->>>>>>> 86b8d19d84a5cf6ceeba3be1ead0bc615ef7344a
 });
--- resources/scripts/player/init.js
+++ resources/scripts/player/init.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
     $.holdReady(true);
 
@@ -16,23 +15,4 @@
 
     script.src = "data/document.js";
     window.document.head.appendChild(script);
-=======
-(function () {
-    $.holdReady(true);
-
-    var script = window.document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.onload = script.onreadystatechange = function (e, isAbort) {
-        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
-            script.onload = script.onreadystatechange = null;
-            script = undefined;
-        }
-
-        if (!isAbort) { $.holdReady(false); }
-    }
-
-    script.src = "data/document.js";
-    window.document.head.appendChild(script);
->>>>>>> 86b8d19d84a5cf6ceeba3be1ead0bc615ef7344a
 })();
